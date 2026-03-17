dab4b7f236314c038e8ccfbc5fdcf9207dfb589c
Joseph Essomba <josephjem2006@gmail.com>
2026-03-17 16:52:09 -0500

    Change resource to data block for resource group

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-resource "azurerm_resource_group" "platform" {
-  name     = "rg-platform-management-tf"
-  location = "eastus"
+data "azurerm_resource_group" "platform" {
+  name = "rg-platform-management"
 }
